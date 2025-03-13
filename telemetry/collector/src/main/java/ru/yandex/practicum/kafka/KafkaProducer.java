2fd076d0ac58dd8c07ae759ae830b0425fb5f660
aperanto-lefko <polarster@yandex.ru>
2025-03-13 18:36:16 +0300

    Создание маппера SensorEvent

---
+++ telemetry/collector/src/main/java/ru/yandex/practicum/kafka/KafkaProducer.java
@@ -0,0 +1,8 @@
+package ru.yandex.practicum.kafka;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class KafkaProducer {
+
+}
--- telemetry/collector/src/main/java/ru/yandex/practicum/mapper/HubEventMapper.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/mapper/HubEventMapper.java
@@ -2,6 +2,7 @@ package ru.yandex.practicum.mapper;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 import org.mapstruct.SubclassMapping;
 import ru.yandex.practicum.kafka.telemetry.event.DeviceAddedEventAvro;
 import ru.yandex.practicum.kafka.telemetry.event.DeviceRemovedEventAvro;
@@ -17,9 +18,10 @@ import ru.yandex.practicum.model.hub.scenario.ScenarioRemovedEvent;
 @Mapper(componentModel = "spring")
 public interface HubEventMapper {
 
-    @Mapping(target = "event", source = ".")
+    @Mapping(target = "event", source = ".", qualifiedByName = "mapClass")
     HubEventAvro mapToAvro(HubEvent event);
 
+    @Named("mapClass")
     @SubclassMapping(source = DeviceAddedEvent.class, target = DeviceAddedEventAvro.class)
     @SubclassMapping(source = DeviceRemovedEvent.class, target = DeviceRemovedEventAvro.class)
     @SubclassMapping(source = ScenarioAddedEvent.class, target = ScenarioAddedEventAvro.class)
@@ -27,7 +29,10 @@ public interface HubEventMapper {
     Object mapClass(HubEvent event);
 
     DeviceAddedEventAvro mapToAvro(DeviceAddedEvent event);
+
     DeviceRemovedEventAvro mapToAvro(DeviceRemovedEvent event);
+
     ScenarioAddedEventAvro mapToAvro(ScenarioAddedEvent event);
+
     ScenarioRemovedEventAvro mapToAvro(ScenarioRemovedEvent event);
 }
