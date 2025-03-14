b72301250f62fff51dd822bc21ea538baf5c5c6c
aperanto-lefko <polarster@yandex.ru>
2025-03-14 13:35:45 +0300

    Создание класса EventService, SendMessageException

--- telemetry/collector/src/main/java/ru/yandex/practicum/service/EventServiceImpl.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/service/EventServiceImpl.java
@@ -8,10 +8,14 @@ import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.avro.specific.SpecificRecordBase;
 import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Service;
+import ru.yandex.practicum.exception.SendMessageException;
 import ru.yandex.practicum.mapper.HubEventMapper;
 import ru.yandex.practicum.mapper.SensorEventMapper;
+import ru.yandex.practicum.model.hub.HubEvent;
+import ru.yandex.practicum.model.sensor.SensorEvent;
 
 @Service
 @RequiredArgsConstructor
@@ -27,6 +31,23 @@ public class EventServiceImpl {
     final HubEventMapper hMp;
     final SensorEventMapper sMp;
 
-    public void
+    public void sendSensorEvent (SensorEvent ev) {
+        SpecificRecordBase avroEvent = sMp.mapToAvro(ev);
+        sendEvent(avroEvent, sensorEventsTopic);
+    }
+    public void sendHubEvent (HubEvent ev) {
+        SpecificRecordBase avroEvent = hMp.mapToAvro(ev);
+        sendEvent(avroEvent, hubEventsTopic);
+    }
+
+    public void sendEvent(SpecificRecordBase avroEvent, String topic) {
+        try {
+            kPr.send(new ProducerRecord<>(topic, avroEvent));
+            log.info("Отправлено сообщение {} в топик  {}", avroEvent, topic);
+        } catch (Exception ex) {
+            log.error("Ошибка при отправлении сообщения:", ex );
+            throw new SendMessageException("Ошибка при отправлении сообщения", ex );
+        }
+    }
 
 }
