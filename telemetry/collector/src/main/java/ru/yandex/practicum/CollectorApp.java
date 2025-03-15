509a93cce49bf6863f3bee4ecea27be6ae8d7ab2
aperanto-lefko <polarster@yandex.ru>
2025-03-15 18:25:03 +0300

    fix pom, dockercompose

--- telemetry/collector/src/main/java/ru/yandex/practicum/CollectorApp.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/CollectorApp.java
@@ -2,11 +2,12 @@ package ru.yandex.practicum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.ComponentScan;
 
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"telemetry"})
+@ConfigurationPropertiesScan
 public class CollectorApp {
     public static void main(String[] args) {
         SpringApplication.run(CollectorApp.class, args);
