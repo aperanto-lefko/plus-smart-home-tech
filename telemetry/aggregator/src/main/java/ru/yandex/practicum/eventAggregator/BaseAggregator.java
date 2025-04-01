81a837f41a8155ddcf12cc4c58248c151c282f3f
aperanto-lefko <polarster@yandex.ru>
2025-04-01 22:33:35 +0300

    Замена на executorservice в BaseAggregator

--- telemetry/aggregator/src/main/java/ru/yandex/practicum/eventAggregator/BaseAggregator.java
+++ telemetry/aggregator/src/main/java/ru/yandex/practicum/eventAggregator/BaseAggregator.java
@@ -20,6 +20,9 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -31,31 +34,46 @@ public abstract class BaseAggregator<K, V, R> {
     protected KafkaConsumer<K, V> consumer;
     private final SnapshotHandler<R> snapshotHandler;
     private static final Duration CONSUME_TIMEOUT = Duration.ofMillis(100);
+    private static final int SHUTDOWN_TIMEOUT_SECONDS = 60;
+    private static final int BATCH_SIZE = 10; // размер батча для коммита
     private volatile boolean running = true; //флаг для управления основным циклом обработки
     private final AtomicBoolean processing = new AtomicBoolean(false); //Чтобы понимать, идет ли сейчас обработка сообщений
     protected abstract List<String> getInputTopics(); //возвращает топики для подписки
     protected abstract Optional<R> processRecord(V record); //обработка одного сообщения
     private final ConcurrentHashMap<TopicPartition, OffsetAndMetadata> offsetsToCommit = new ConcurrentHashMap<>();
-    private static final int BATCH_SIZE = 10; // размер батча для коммита
     private final AtomicInteger processedInBatch = new AtomicInteger(0);
+    private final ExecutorService executorService = Executors.newSingleThreadExecutor();
 
-    @PostConstruct //запускается при инициализации бина Spring
+    @PostConstruct
     public void start() {
         log.info("Подписка на топики: {}", getInputTopics());
         consumer.subscribe(getInputTopics());
-        new Thread(this::pollLoop).start();
+        executorService.submit(this::pollLoop);
     }
 
     @PreDestroy
     public void shutdown() {
-        log.info("Фиксация оставшихся офсетов перед окончанием работы");
-        if(!offsetsToCommit.isEmpty()) {
-            commitOffsets(offsetsToCommit);
-        }
-        log.info("Завершение работы агрегатора");
+        log.info("Начало завершения работы агрегатора...");
         running = false;
-        waitForCompletion();
+        // Остановка ExecutorService
+        executorService.shutdown();
+        try {
+            if (!executorService.awaitTermination(SHUTDOWN_TIMEOUT_SECONDS, TimeUnit.SECONDS)) {
+                log.info("Принудительное завершение после таймаута");
+                executorService.shutdownNow();
+                waitForCompletion();
+            }
+        } catch (InterruptedException e) {
+            log.error("Работа прервана interrupt", e);
+            executorService.shutdownNow();
+            Thread.currentThread().interrupt();
+        }
+        // Фиксация оставшихся офсетов
+        log.info("Фиксация оставшихся офсетов");
+        commitOffsets(offsetsToCommit);
+        // Завершение работы consumer
         consumer.wakeup();
+        log.info("Агрегатор успешно остановлен");
     }
 
     private void pollLoop() {
@@ -98,17 +116,21 @@ public abstract class BaseAggregator<K, V, R> {
         }
     }
 
-   private void commitOffsets(Map<TopicPartition, OffsetAndMetadata> offsetsToCommit) {
-       try {
-           if (!offsetsToCommit.isEmpty()) {
-               consumer.commitSync(offsetsToCommit);
-               log.info("Офсеты успешно зафиксированы для {} партиций", offsetsToCommit.size());
-               offsetsToCommit.clear();
-           }
-       } catch (CommitFailedException e) {
-           log.error("Ошибка фиксации офсетов для {} партиций", offsetsToCommit.size(), e);
-       }
-   }
+    private void commitOffsets(Map<TopicPartition, OffsetAndMetadata> offsetsToCommit) {
+        try {
+            if (!offsetsToCommit.isEmpty()) {
+                consumer.commitSync(offsetsToCommit);
+                log.info("Офсеты успешно зафиксированы для {} партиций", offsetsToCommit.size());
+                offsetsToCommit.clear();
+            }
+        } catch (CommitFailedException e) {
+            log.error("Ошибка фиксации офсетов для {} партиций", offsetsToCommit.size(), e);
+        } catch (WakeupException e) {
+            log.warn("Коммит прерван WakeupException");
+            throw e; // Пробрасываем для обработки в pollLoop
+        }
+    }
+
     /*
      * Ожидает завершения текущей обработки при shutdown
      */
@@ -126,7 +148,7 @@ public abstract class BaseAggregator<K, V, R> {
     private void closeResources() {
         try {
             log.info("Завершающая фиксация офсетов...");
-            consumer.commitSync();
+            commitOffsets(offsetsToCommit);
             log.info("Закрытие консьюмера...");
             consumer.close(Duration.ofSeconds(5));
         } catch (Exception e) {
