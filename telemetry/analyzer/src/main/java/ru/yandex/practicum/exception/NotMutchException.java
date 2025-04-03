d7ebbcf3de29409242e693c417e64280f4ca7329
aperanto-lefko <polarster@yandex.ru>
2025-04-03 18:01:14 +0300

    Создание предварительной модели для analyzer

---
+++ telemetry/analyzer/src/main/java/ru/yandex/practicum/exception/NotMutchException.java
@@ -0,0 +1,7 @@
+package ru.yandex.practicum.exception;
+
+public class NotMutchException extends RuntimeException {
+    public NotMutchException(String message) {
+        super(message);
+    }
+}
