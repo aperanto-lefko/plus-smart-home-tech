d4cb5eee5aa50377d3f777214d4cada6fb473093
aperanto-lefko <polarster@yandex.ru>
2025-04-04 14:46:37 +0300

    Изменение класса BaseAggregator

--- telemetry/kafka-commons/src/main/java/ru/yandex/practicum/receiver/BaseAggregator.java
+++ telemetry/kafka-commons/src/main/java/ru/yandex/practicum/receiver/BaseAggregator.java
@@ -1,32 +1,25 @@
-package ru.yandex.practicum.aggregator;
+package ru.yandex.practicum.receiver;
 
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
-import org.springframework.stereotype.Component;
-import ru.yandex.practicum.handler.SnapshotHandler;
-import ru.yandex.practicum.receiver.KafkaConsumerManager;
-import ru.yandex.practicum.receiver.OffsetCommitManager;
-import ru.yandex.practicum.record_process.RecordProcessor;
+import ru.yandex.practicum.kafka.telemetry.event.SensorEventAvro;
+
 
 import java.util.List;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.function.Consumer;
 
 @Slf4j
-@Component
 @RequiredArgsConstructor
-public abstract class BaseAggregator<K, V, R> {
+public abstract class BaseAggregator<K, V> {
     protected final KafkaConsumerManager<K, V> consumerManager;
-    protected final RecordProcessor<V, R> recordProcessor;
     protected final OffsetCommitManager<K, V> offsetCommitManager;
-    protected final SnapshotHandler<R> snapshotHandler;
     protected final AtomicBoolean processing = new AtomicBoolean(false);
 
     protected abstract List<String> getInputTopics();
-
     protected abstract Consumer<ConsumerRecords<K, V>> createBatchProcessor();
 
     @PostConstruct
