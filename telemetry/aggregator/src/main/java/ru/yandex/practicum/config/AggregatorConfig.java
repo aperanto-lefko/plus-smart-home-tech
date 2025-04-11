f79b25a7ea61d83721f92092903d14926787eea0
aperanto-lefko <polarster@yandex.ru>
2025-04-11 18:15:15 +0300

    создание ветки 5-config-server, перенос настроек агрегатора

--- telemetry/aggregator/src/main/java/ru/yandex/practicum/config/AggregatorConfig.java
+++ telemetry/aggregator/src/main/java/ru/yandex/practicum/config/AggregatorConfig.java
@@ -5,6 +5,7 @@ import org.apache.kafka.clients.consumer.KafkaConsumer;
 
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import ru.yandex.practicum.receiver.KafkaConsumerManager;
@@ -20,6 +21,7 @@ import java.util.concurrent.ExecutorService;
 
 @Configuration
 @RequiredArgsConstructor
+@ConfigurationProperties(prefix="aggregator.kafka")
 public class AggregatorConfig {
     private final KafkaConsumerFactory factory;
     private final SnapshotHandler<SensorsSnapshotAvro> snapshotHandler;
