b5d1a189f27e1883323013712184731db948236e
aperanto-lefko <polarster@yandex.ru>
2025-04-14 18:11:04 +0300

    Создание Dto для product, создание schema, редакция pom и application для модулей

---
+++ commerce/interaction-api/src/main/java/ru/yandex/practicum/store/dto/ProductDto.java
@@ -0,0 +1,38 @@
+package ru.yandex.practicum.store.dto;
+
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import ru.yandex.practicum.store.enums.ProductCategory;
+import ru.yandex.practicum.store.enums.ProductState;
+import ru.yandex.practicum.store.enums.QuantityState;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Data
+@RequiredArgsConstructor
+@AllArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class ProductDto {
+    UUID productId;
+    @NotBlank
+    String productName;
+    @NotBlank
+    String description;
+    String imageSrc;
+    @NotBlank(message = "Поле \"Состояние остатка\" не должно быть пустым")
+    QuantityState quantityState;
+    @NotBlank(message = "Поле \"Статус товара\" не должно быть пустым")
+    ProductState productState;
+    @NotBlank(message = "Поле \"Категория товара\" не должно быть пустым")
+    ProductCategory productCategory;
+    @DecimalMin(value = "1.0", message = "Цена товара должна быть не меньше 1")
+    BigDecimal price;
+}
